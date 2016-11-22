0f0863148d18681e6938dd2618997d6a385715b3
mauriciojuba <mauriciojuba@outlook.com>
2016-11-22 15:47:38 -0200

    Rain out aiMyself

---
+++ Assets/Scripts/AIOne.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIOne : MonoBehaviour {
+
+    Vector3 player;
+    Vector2 playerDirection;
+    float Xdif, Ydif;
+    public float speed;
+    private int Wall;
+    float distance;
+    public float distanceToChase;
+	void Start () {
+        Wall = 1 << 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Vector2.Distance(player, transform.position);
+        player = GameObject.Find("Player").transform.position;
+
+        if (distance < distanceToChase)
+        {
+            Xdif = player.x - transform.position.x;
+            Ydif = player.y - transform.position.y;
+            playerDirection = new Vector2(Xdif, Ydif);
+
+            if (!Physics2D.Raycast(transform.position, playerDirection, 3, Wall))
+            {
+                GetComponent<Rigidbody2D>().AddForce(playerDirection.normalized * speed);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    Transform target;
+
+	void Start () {
+        target = GameObject.Find("Player").transform;
+	}
+	void Update () {
+        transform.position = target.position + new Vector3(0, 0, -10);
+	}
+}
