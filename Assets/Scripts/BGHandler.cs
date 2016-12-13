1145cc6edf7353fe7e7008ea41a045557a97ace3
mauriciojuba <mauriciojuba@outlook.com>
2016-12-13 01:16:10 -0200

    Sounds and Sprites

---
+++ Assets/Scripts/BGHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGHandler : MonoBehaviour {
+
+    // Use this for initialization
+    Transform cam;
+    public float distZ;
+
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").transform;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z + distZ);
+    }
+}
